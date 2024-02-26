e9bea055f2d7323f80c85f7b480cdbbda3088b36
andrepcg <andrepcg@gmail.com>
2024-02-26 17:13:36 +0000

    CF Worker now works

--- workers/marcar-promessa-cumprida/src/index.js
+++ workers/marcar-promessa-cumprida/src/index.js
@@ -1,31 +1,37 @@
-// const octokit = new Octokit({
-//   auth: 'YOUR-TOKEN'
-// })
+import { Octokit, App } from "octokit";
 
-// await octokit.request('POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', {
-//   owner: process.env.REPO_OWNER,
-//   repo: process.env.REPO,
-//   workflow_id: process.env.WORKFLOW_FILE_NAME,
-//   ref: process.env.REPO_MAIN_BRANCH,
-//   inputs: {
-//     doc_path: 'Mona the Octocat',
-//     fulfilled_date: 'San Francisco, CA',
-//     links
-//   },
-//   headers: {
-//     'X-GitHub-Api-Version': '2022-11-28'
-//   }
-// })
 
 export default {
-  async fetch(request, env, ctx) {
-    if (request.method !== "POST") {
-      return new Response("Invalid request");
+	async fetch(request, env, ctx) {
+		const octokit = new Octokit({ auth: env.OCTOKIT_TOKEN })
+
+		if (request.method !== "POST") {
+			return new Response("Invalid request", { status: 400 });
     }
 
     const json = await request.json()
-    return Response.json(json);
 
-    // return Response.json(data);
+		if (!json.doc_path || !json.fulfilled_date || !json.links) {
+			return new Response("Missing inputs", { status: 400 });
+		}
+
+		console.log("Received", json);
+
+		if (isNaN(new Date(json.fulfilled_date)) || json.links.split(",").length === 0) {
+			return new Response("Invalid inputs", { status: 400 });
+		}
+
+		// const gh_response = await octokit.request('POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', {
+		// 	owner: env.REPO_OWNER,
+		// 	repo: env.REPO,
+		// 	workflow_id: env.WORKFLOW_FILE_NAME,
+		// 	ref: env.REPO_MAIN_BRANCH,
+		// 	inputs: json,
+		// 	headers: {
+		// 		'X-GitHub-Api-Version': '2022-11-28'
+		// 	}
+		// })
+
+    return new Response(null, { status: 204 });
   },
 };
