c750e28c09d2056063195cd68ec9b539bcfcf445
andrepcg <andrepcg@gmail.com>
2024-02-27 15:33:53 +0000

    Replace link with A

--- app/page.js
+++ app/page.js
@@ -1,5 +1,3 @@
-import Link from 'next/link';
-
 import { getSections } from '@/lib/promessas';
 import { PARTY_NAME, PARTY } from '../consts';
 import Section from '@/app/_components/section';
@@ -12,7 +10,7 @@ export default async function Home() {
       <div className='text-center mb-12'>
         <h1 className='text-4xl mb-4'>Parabéns, <strong>{PARTY_NAME}</strong> 2024!</h1>
         <h3 className='text-xl mb-4'>Agora está na altura de cumprir.<br/>Como dizia o outro, <span className='italic'>&quot;<s>Paga</s> Cumpre o que deves!&quot;</span></h3>
-        <Link className='underline' href={`/programas/${PARTY}.pdf`}>Programa</Link>
+        <a className='underline' href={`/programas/${PARTY}.pdf`}>Programa</a>
       </div>
 
       <div>
