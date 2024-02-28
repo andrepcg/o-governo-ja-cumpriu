693b44c786b952a103846039c1128f4bb884902f
andrepcg <andrepcg@gmail.com>
2024-02-28 10:58:18 +0000

    Hide arrow when not hovering link

--- app/_components/promessa-small.jsx
+++ app/_components/promessa-small.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export default function PromessaSmall({ urlPath, data: { fulfilled_date, sub_section }, content }) {
   return (
-    <Link href={urlPath} className="promessa mb-6 py-2 block rounded-md border border-transparent hover:border-gray-200">
+    <Link href={urlPath} className="promessa mb-6 py-2 block rounded-md border border-transparent group hover:border-gray-200">
       <div className="flex flex-row items-center">
         <div href={urlPath} className="p-4">{fulfilled_date ? '✅' : '❌'}</div>
         <div className="grow">
@@ -14,7 +14,7 @@ export default function PromessaSmall({ urlPath, data: { fulfilled_date, sub_sec
           )}
           <p className="" dangerouslySetInnerHTML={{ __html: content }} />
         </div>
-        <div className="p-4 text-gray-400 block">
+        <div className="p-4 text-gray-400 block invisible group-hover:visible">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
           </svg>
