376dfa5fb27973e598bd8d349997b4bf32c67512
Andre Goncalves <andrepcg@gmail.com>
2024-03-03 19:32:25 +0000

    T&C e PP

--- app/_components/search.jsx
+++ app/_components/search.jsx
@@ -76,8 +76,8 @@ const SearchBox = ({ onFocus, onChange }) => {
 
   return (
     <form className="mx-auto w-2/3">
-      <div class="relative w-full">
-        <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+      <div className="relative w-full">
+        <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-black">
             <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
           </svg>
