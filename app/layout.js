6721be0b607690b4e27b4995340173b42361894e
andrepcg <andrepcg@gmail.com>
2024-03-04 14:14:18 +0000

    Change html lang to PT

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="pt">
       <head>
         <meta name="theme-color" content="#000" />
         <meta name="description" content="A seguir as promessas eleitorais do Governo Português desde 2024"></meta>
@@ -26,7 +26,6 @@ export default function RootLayout({ children }) {
         <meta property="og:image:height" content="630" />
       </head>
       <body className="bg-white">
-        {/* <Header /> */}
         <main className="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900 antialiased">
           <div className="px-4 mx-auto max-w-screen-lg ">
             {/* <NavBar /> */}
