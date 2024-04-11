b5de7f4f84d618558aa63896e3a199cb326e98ef
andrepcg <andrepcg@gmail.com>
2024-04-11 10:42:27 +0100

    Remove function from utils

--- app/layout.js
+++ app/layout.js
@@ -6,8 +6,6 @@ import "./styles/global.css";
 
 import NavBar from '@/app/_components/nav-bar'
 
-import { injectLfTracker } from '@/lib/utils';
-
 const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
 const LF_TRACKING_ID = process.env.NEXT_PUBLIC_LF_ID;
 
@@ -26,7 +24,15 @@ export default function RootLayout({ children }) {
         <meta property="og:image" content="https://ogovernojacumpriu.pt/og.png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
-        {LF_TRACKING_ID && injectLfTracker(LF_TRACKING_ID)}
+        {LF_TRACKING_ID && (
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(ss,ex){window.ldfdr=window.ldfdr||function(){(ldfdr._q=ldfdr._q||[]).push([].slice.call(arguments));}; (function(d,s){ fs=d.getElementsByTagName(s)[0]; function ce(src){ var cs=d.createElement(s); cs.src=src; cs.async=1; fs.parentNode.insertBefore(cs,fs); }; ce('https://sc.lfeeder.com/lftracker_v1_'+ss+(ex?'_'+ex:'')+'.js'); })(document,'script'); })('${LF_TRACKING_ID}');
+              `
+            }}>
+          </script>
+        )}
       </head>
       <body className="bg-white">
         <main className="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900 antialiased">
