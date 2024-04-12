eca0bb5bae87a222b87c6de69ee5d565cf82e0f3
André Perdigão Gonçalves <andrepcg@gmail.com>
2024-04-12 20:15:09 +0100

    Uso do programa do governo correcto (#9)

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import { getSections } from '@/lib/promessas';
-import { PARTY_NAME, PARTY } from '../consts';
+import { PARTY_NAME } from '../consts';
 import Section from '@/app/_components/section';
 
 export default async function Home() {
@@ -10,7 +10,7 @@ export default async function Home() {
       <div className='text-center mb-12'>
         <h1 className='text-4xl mb-4'>Parabéns, <strong>{PARTY_NAME}</strong> 2024!</h1>
         <h3 className='text-xl mb-4'>Agora está na altura de cumprir.<br/>Como dizia o outro, <span className='italic'>&quot;<s>Paga</s> Cumpre o que deves!&quot;</span></h3>
-        <a className='underline' href={`/programas/${PARTY}.pdf`}>Programa</a>
+        <a className='underline' href="/programas/programa.pdf">Programa</a>
       </div>
 
       <div>
