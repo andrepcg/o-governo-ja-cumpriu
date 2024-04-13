bd9b474112826edcdcf0cbf68a8afdd034d783f0
Andre Goncalves <andrepcg@gmail.com>
2024-04-13 09:57:04 +0100

    Remove funny header

--- app/page.js
+++ app/page.js
@@ -9,7 +9,6 @@ export default async function Home() {
     <div>
       <div className='text-center mb-12'>
         <h1 className='text-4xl mb-4'>Parabéns, <strong>{PARTY_NAME}</strong> 2024!</h1>
-        <h3 className='text-xl mb-4'>Agora está na altura de cumprir.<br/>Como dizia o outro, <span className='italic'>&quot;<s>Paga</s> Cumpre o que deves!&quot;</span></h3>
         <a className='underline' href="/programas/programa.pdf">Programa</a>
       </div>
 
