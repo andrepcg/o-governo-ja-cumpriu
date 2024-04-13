58158157213654e61486de5a5e6c97a49f8013f3
Andre Goncalves <andrepcg@gmail.com>
2024-04-13 09:40:30 +0100

    Add JSON-LD

--- app/s/[slug]/[id]/page.js
+++ app/s/[slug]/[id]/page.js
@@ -1,4 +1,5 @@
 import { notFound } from "next/navigation";
+import Link from 'next/link'
 
 import { getPromessasData, getSectionPromessa } from '@/lib/promessas';
 import { pullRequestsForDocument } from '@/lib/github';
@@ -6,8 +7,9 @@ import { pullRequestsForDocument } from '@/lib/github';
 import MarcarPromessaCumprida from '@/app/_components/marcar-promessa-cumprida';
 
 async function GithubPullRequests({ docPath }) {
-  const prs = await pullRequestsForDocument(docPath)
+  // const prs = await pullRequestsForDocument(docPath)
 
+  const prs = []
   if (prs.length === 0) {
     return <MarcarPromessaCumprida docPath={docPath} />
   }
@@ -27,6 +29,37 @@ async function GithubPullRequests({ docPath }) {
   )
 }
 
+function generateJsonLd(promessa, slug) {
+  const { data: { section, sub_section, title }, content } = promessa;
+
+  const data = [
+    {
+      "@context": "https://schema.org",
+      "@type": "NewsArticle",
+      "headline": title,
+      "articleBody": content,
+      articleSection: `${section} / ${sub_section}`
+    },
+    {
+      "@context": "https://schema.org",
+      "@type": "BreadcrumbList",
+      "itemListElement": [{
+        "@type": "ListItem",
+        "position": 1,
+        "name": title,
+        "item": `https://ogovernojacumpriu.pt/s/${slug}`
+      },{
+        "@type": "ListItem",
+        "position": 2,
+        "name": sub_section
+      }]
+    }
+  ]
+  return (
+    <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }} />
+  )
+}
+
 export default async function Promessa({ params: { slug, id }}) {
   const promessa = getSectionPromessa(slug, id)
 
@@ -36,12 +69,12 @@ export default async function Promessa({ params: { slug, id }}) {
 
   const { data: { fulfilled_date, links_to_news_articles, section, sub_section }, content, docPath } = promessa;
   return (
-    <article>
-      <h1 className="text-3xl font-bold">{section}</h1>
-      {sub_section && <h4 className="text-xl text-gray-400">{sub_section}</h4>}
+    <article itemtype="https://schema.org/NewsArticle">
+      <Link href={`/s/${slug}`}><h1 className="text-3xl font-bold">{section}</h1></Link>
+      {sub_section && <h4 className="text-xl text-gray-400">{sub_section} • {id}</h4>}
 
       <blockquote className="my-6 p-4 border-s-4 border-gray-300 bg-gray-50 dark:border-gray-500 dark:bg-gray-800">
-        <p className="italic font-medium leading-relaxed text-gray-900 dark:text-white" dangerouslySetInnerHTML={{ __html: content }}/>
+        <p id="main" className="italic font-medium leading-relaxed text-gray-900 dark:text-white" dangerouslySetInnerHTML={{ __html: content }}/>
       </blockquote>
 
       <p><strong>Cumprida?</strong> {fulfilled_date ? `✅ (${fulfilled_date.toDateString()})` : '❌'}</p>
@@ -59,6 +92,7 @@ export default async function Promessa({ params: { slug, id }}) {
           </ul>
         </>
       )}
+      {generateJsonLd(promessa, slug)}
     </article>
   )
 }
